2a74ba8376ab572052cda334446a4447051894ee
Origin2019 <241880344@smail.nju.edu.cn>
2026-05-26 21:05:06 +0800

    Update OrderMapper.java

--- sec-ii-2026/backend/src/main/java/com/example/backend/mapper/OrderMapper.java
+++ sec-ii-2026/backend/src/main/java/com/example/backend/mapper/OrderMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface OrderMapper {
