fa08c7dc314e42e4c809bd3e5e6b3e771a127842
Origin2019 <241880344@smail.nju.edu.cn>
2026-05-27 20:06:46 +0800

    Delete sec-ii-2026 directory

--- sec-ii-2026/backendC/src/main/java/com/example/backend/model/enums/OrderStatus.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.model.enums;
-
-public enum OrderStatus {
-    accepted,    // 已接单
-    completed,   // 已完成（需求方确认送达）
-    cancelled,   // 已取消
-    evaluated    // 已评价（双方互评结束，订单最终完结终态）
-}
