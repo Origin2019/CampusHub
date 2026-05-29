f00d2e6b60eae8f2e4536f98998fd25a625b95b8
Origin2019 <241880344@smail.nju.edu.cn>
2026-05-29 11:14:07 +0800

    Delete C/backendC directory

--- C/backendC/src/main/java/com/example/backend/model/enums/TaskStatus.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.model.enums;
-
-public enum TaskStatus {
-    published,   // 已发布
-    in_progress, // 进行中
-    completed,   // 已完成
-    cancelled,   // 已取消
-    evaluated    // 已评价完结
-}
